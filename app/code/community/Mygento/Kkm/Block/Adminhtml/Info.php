4ae64d3308603121df507a94bc304d5ef67ab8d7
Dmitrii Gromov <d.gromov@mygento.ru>
2017-06-28 16:36:24 +0300

    [NN-74] added discount helper version

---
+++ app/code/community/Mygento/Kkm/Block/Adminhtml/Info.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ *
+ * @category Mygento
+ * @package Mygento_Kkm
+ * @copyright Copyright © 2016 NKS LLC. (http://www.mygento.ru)
+ */
+class Mygento_Kkm_Block_Adminhtml_Info extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
+{
+    /**
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     *
+     * @SuppressWarnings("unused")
+     */
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $helper = Mage::helper('kkm');
+        $html   = '<tr><td class="label">' . $helper->__('Discount Helper Version:') . '</td><td class="value" style="font-weight: bold;">' .
+            Mygento_Kkm_Helper_Discount::VERSION . '</td></tr>';
+
+
+        return $html;
+    }
+
+}
