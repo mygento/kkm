41f8220a72cd4db42fa3cf958bfeb543b5daa902
Ruslan Mavlyanov <r.mavlyanov@mygento.ru>
2018-01-25 16:23:22 +0300

    [NN-259] Added ability to set modes of Discount.php from module config

--- app/code/community/Mygento/Kkm/Model/Vendor/Atol.php
+++ app/code/community/Mygento/Kkm/Model/Vendor/Atol.php
@@ -377,10 +377,10 @@ class Mygento_Kkm_Model_Vendor_Atol extends Mygento_Kkm_Model_Abstract
             $receipt->getOrder()->setShippingDescription($this->getConfig('general/custom_shipping_name'));
         }
 
-
-        $algorithm = $this->getConfig('general/apply_algorithm');
-        if (!$algorithm) {
-            $discountHelper->setDoCalculation(false);
+        $discountHelper->setDoCalculation(boolval($this->getConfig('general/apply_algorithm')));
+        if ($this->getConfig('general/apply_algorithm')) {
+            $discountHelper->setSpreadDiscOnAllUnits(boolval($this->getConfig('general/spread_discount')));
+            $discountHelper->setIsSplitItemsAllowed(boolval($this->getConfig('general/split_allowed')));
         }
 
         $recalculatedReceiptData          = $discountHelper->getRecalculated($receipt, $tax_value, $attribute_code, $shipping_tax);
