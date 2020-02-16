237318cfb202b8f2669281c7c619954820c52d2c
Dmitrii Chukhnov <d.chukhnov@yandex.ru>
2020-02-16 18:44:43 +0300

    NN2-2201 - add UserProp class and additional_user_props for Atol request.

---
+++ Api/Data/UserPropInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author Mygento Team
+ * @copyright 2017-2019 Mygento (https://www.mygento.ru)
+ * @package Mygento_Kkm
+ */
+
+namespace Mygento\Kkm\Api\Data;
+
+interface UserPropInterface
+{
+    /**
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name);
+
+    /**
+     * @return string
+     */
+    public function getValue(): string;
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setValue(string $value);
+}
