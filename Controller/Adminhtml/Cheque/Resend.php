695978831e3b649157549c2c2d6af3f9e7ac3e4d
Valery Okrugin <v.okrugin@mygento.ru>
2020-10-09 13:53:39 +0300

    NN2-3475 Not ignore trials by manual send

--- Controller/Adminhtml/Cheque/Resend.php
+++ Controller/Adminhtml/Cheque/Resend.php
@@ -81,12 +81,12 @@ class Resend extends \Magento\Backend\App\Action
             switch ($entityType) {
                 case 'invoice':
                     $entity = $this->invoiceRepository->get($id);
-                    $this->processor->proceedSell($entity, false, true);
+                    $this->processor->proceedSell($entity);
                     $comment = 'Cheque ';
                     break;
                 case 'creditmemo':
                     $entity = $this->creditmemoRepository->get($id);
-                    $this->processor->proceedRefund($entity, false, true);
+                    $this->processor->proceedRefund($entity);
                     $comment = 'Refund ';
                     break;
             }
